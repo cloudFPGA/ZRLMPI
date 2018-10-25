bc189caf45c4c0a207ec53411c908ffcd052e844
Burkhard Ringlein <ngl@zurich.ibm.com>
2018-10-25 16:42:05 +0000

    changed to new test case 64x64

--- ZRLMPI/test.cpp
+++ ZRLMPI/test.cpp
@@ -1,13 +1,10 @@
 
+#include <stdlib.h>
 #include <stdio.h>
-#include <stdint.h>
 #include <math.h>
-
-#include "test.hpp"
 #include "MPI.hpp"
+#include "test.hpp"
 
-
-//for debugging
 
 void print_array(const int *A, size_t width, size_t height)
 {
@@ -23,19 +20,12 @@ void print_array(const int *A, size_t width, size_t height)
   printf("\n");
 }
 
-
-
-//int main( int argc, char **argv )
-//DUE TO SHITTY HLS...
-void app_main()
+int app_main()
 {
-  MPI_Init();
-  //MPI_Init(&argc, &argv);
-
-
   int        rank, size;
   MPI_Status status;
 
+  MPI_Init();
 
   MPI_Comm_rank( MPI_COMM_WORLD, &rank );
   MPI_Comm_size( MPI_COMM_WORLD, &size );
@@ -43,71 +33,157 @@ void app_main()
 
   printf("Here is rank %d, size is %d. \n",rank, size);
 
-	    int grid[DIM][DIM];
+  if(rank == 0)
+  {// Master ...
+    int grid[DIM][DIM];
 
-	    //fill with zeros and init borders with 1
-	    for(int i = 0; i<DIM; i++)
-	    {
-	      for(int j = 0; j<DIM; j++)
-	      {
+    //fill with zeros and init borders with 1
+    for(int i = 0; i<DIM; i++)
+    {
+      for(int j = 0; j<DIM; j++)
+      {
 
-	        if( i == 0 || i == DIM-1 || j == 0 || j == DIM-1)
-	        {
-	          grid[i][j] = 4;
-	        }
-	        else {
-	          grid[i][j] = 0;
-	        }
-	      }
-	    }
-	
+        if( i == 0 || i == DIM-1 || j == 0 || j == DIM-1)
+        {
+          grid[i][j] = CORNER_VALUE_INT;
+        }
+        else {
+          grid[i][j] = 0;
+        }
+      }
+    }
+
     print_array((const int*) grid, DIM, DIM);
 
+    printf("Ditstribute data and start client nodes.\n");
 
-	    printf("Ditstribute data and start client nodes.\n");
 
+    for(int i = 1; i<size; i++)
+    {
+      //distribute data ONLY in PACKETLENGTH packets
+      int first_send_line = (i-1)*LDIMY;
+      if ( first_send_line != 0)
+      {
+        first_send_line -= 2; //for borders
+      }
+      //int last_send_line = i*LDIMY;
+      //if(i == size-1)
+      //{
+      //  //first_send_line = (LDIMY-2);
+      //  last_send_line--;
+      //}
+      //for(int j = first_send_line; j<=last_send_line; j++)
+      //{
+        printf("Sending line %d to rank %d.\n", first_send_line, i);
+        MPI_Send(&grid[first_send_line][0], PACKETLENGTH, MPI_INTEGER, i, 0, MPI_COMM_WORLD);
+      //}
+    }
 
-	    for(int i = 1; i<size; i++)
-	    {
-	      //distribute data
-	      int first_send_line = 0;
-	      if(i == size-1)
-	      {
-	        first_send_line = (LDIMY-2);
-	      }
-	      MPI_Send(&grid[first_send_line][0], LDIMX*LDIMY, MPI_INTEGER, i, 0, MPI_COMM_WORLD);
-	    }
+    int tmp[LDIMY+1][LDIMX];
 
-	    int tmp[LDIMY][LDIMX];
+    for(int i = 1; i<size; i++)
+    {
+      //collect results
+      //int number_of_recv_packets = LDIMY + 1; 
+      //if(i == size -1)
+      //{
+      //  number_of_recv_packets--;
+      //}
+      //for(int j = 0; j< number_of_recv_packets; j++)
+      //{
+       MPI_Recv(&tmp[0][0], PACKETLENGTH, MPI_INTEGER, i, 0, MPI_COMM_WORLD, &status);
+      //}
 
-	    for(int i = 1; i<size; i++)
-	    {
-	      //collect results
-	      MPI_Recv(&tmp[0][0], LDIMY*LDIMX, MPI_INTEGER, i, 0, MPI_COMM_WORLD, &status);
+      int first_recv_line = (i-1)*LDIMY;
+      //int last_recv_line = i*LDIMY + 1;
+      //if(i == size-1)
+      //{
+      //  //first_send_line = (LDIMY-2);
+      //  last_recv_line--;
+      //}
+      if ( first_recv_line != 0)
+      {
+        first_recv_line -= 1; //for borders
+      }
 
-        int first_target_line = 0;
-	      if(i == size-1)
-	      {
-	        first_target_line = LDIMY -2;
-	      }
+      //for(int y = 0; y < number_of_recv_packets; y++)
+      //{
+      //  if(y == 0 && i == 1)
+      //  {
+      //    continue;
+      //  }
+      //  if( y == number_of_recv_packets - 1 && i == size - 1)
+      //  {
+      //    continue;
+      //  }
+      for(int y = 1; y< LDIMY - 1; y++)
+      {
+        for(int x = 1; x < LDIMX-1; x++)
+        {
+          grid[first_recv_line + y][x] = tmp[y][x];
+        }
+      }
 
-	      for(int y = 1; y < LDIMY-1; y++)
-	      {
-	        for(int x = 1; x < LDIMX-1; x++)
-	        {
-	          grid[first_target_line + y][x] = tmp[y][x];
-	        }
-	      }
+    }
 
-	    }
-
-	    printf("Done.\n");
+    printf("Done.\n");
     print_array((const int*) grid, DIM, DIM);
+    
 
-      MPI_Finalize();
+  } else { 
+    //Slaves ... 
 
-  return;
+    int local_grid[LDIMY][LDIMX];
+    int local_new[LDIMY][LDIMX];
+
+    //MPI_Recv(&local_grid[0][0], LDIMY*LDIMX, MPI_INTEGER, 0, 0, MPI_COMM_WORLD, &status);
+    //int number_of_recv_packets = LDIMY + 1; 
+    //if(rank == size -1)
+    //{
+    //  number_of_recv_packets--;
+    //}
+
+    //for(int j = 0; j< LDIMY; j++)
+    //{
+      //if(j == number_of_recv_packets)
+      //{
+      //  break;
+      //}
+      MPI_Recv(&local_grid[0][0], PACKETLENGTH, MPI_INTEGER, 0, 0, MPI_COMM_WORLD, &status);
+    //}
+
+    // print_int_array((const int*) local_grid, LDIMX, LDIMY);
+
+    //only one iteration for now
+    //treat all borders equal, the additional lines in the middle are cut out from the merge at the server
+    for(int i = 1; i < LDIMY - 1; i++)
+    {
+      //if(i == number_of_recv_packets)
+      //{
+      //  break;
+      //}
+      for(int j = 1; j<LDIMX-1; j++)
+      {
+        local_new[i][j] = (local_grid[i][j-1] + local_grid[i][j+1] + local_grid[i-1][j] + local_grid[i+1][j]) / 4.0;
+      }
+    }
+    //MPI_Send(&local_new[0][0], LDIMY*LDIMX, MPI_INTEGER, 0, 0, MPI_COMM_WORLD);
+    //for(int j = 0; j< LDIMY; j++)
+    //{
+      //if(j == number_of_recv_packets)
+      //{
+      //  break;
+      //}
+      MPI_Send(&local_new[0][0], PACKETLENGTH, MPI_INTEGER, 0, 0, MPI_COMM_WORLD);
+    //}
+
+    //print_int_array((const int*) local_new, LDIMX, LDIMY);
+
+    //printf("Calculation finished.\n");
+  }
+
+  MPI_Finalize();
+  return 0;
 }
-
 
 
