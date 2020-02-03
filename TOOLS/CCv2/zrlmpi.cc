fc2278b5fcd8de5750f73eabf10e1d5020f97a2c
Burkhard Ringlein <ngl@zurich.ibm.com>
2020-02-03 18:00:49 +0100

    updated bash scripts for new compiler

--- TOOLS/CCv2/zrlmpi.cc
+++ TOOLS/CCv2/zrlmpi.cc
@@ -13,27 +13,28 @@
 
 
 
-HW_TARGET_C=$4/hls/mpi_wrapperv1/src/app_hw.cpp
-HW_TARGET_H=$4/hls/mpi_wrapperv1/src/app_hw.hpp
+HW_TARGET_C=$5/hls/mpi_wrapperv1/src/app_hw.cpp
+HW_TARGET_H=$5/hls/mpi_wrapperv1/src/app_hw.hpp
 
 SW_TARGET_C=$1/SW/app_sw.cpp
 SW_TARGET_H=$1/SW/app_sw.hpp
 
 OWN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-if [ 4 -ne $# ]
+PY_ENV=$2/py_zrlmpi/
+
+if [ 6 -ne $# ]
 then 
-  echo "USAGE: $0 path/to/cFp_root_dir path/to/MPI/c-file path/to/MPI/h-file path/to/ROLE_DIR"
+  echo "USAGE: $0 path/to/cFp_root_dir path/to/ZRLMPI_dir path/to/MPI/c-file path/to/MPI/h-file path/to/ROLE_DIR path/to/cluster/description_json"
   echo "This asumes that the the mpi_wrapper in ROLE_DIR will be used"
 else 
   make -C $OWN_DIR/unifdef/ 
-  if [[ $2 -nt $HW_TARGET_C ]]; then
-  echo "./zrlmpi.cc1 $2 $HW_TARGET_C $SW_TARGET_C"
-  $OWN_DIR/zrlmpi.cc1 $2 $HW_TARGET_C $SW_TARGET_C
-  fi
-  if [[ $3 -nt $HW_TARGET_H ]]; then
-  echo "./zrlmpi.cc1 $3 $HW_TARGET_H $SW_TARGET_H"
-  $OWN_DIR/zrlmpi.cc1 $3 $HW_TARGET_H $SW_TARGET_H
+  if [[ $3 -nt $HW_TARGET_C ]] || [[ $4 -nt $HW_TARGET_H ]] \
+  || [[ $3 -nt $SW_TARGET_C ]] || [[ $4 -nt $SW_TARGET_H ]] \
+  || [[ $6 -nt $HW_TARGET_C ]] || [[ $6 -nt $HW_TARGET_H ]]; then
+  # the json only influences the HW targets...
+  echo "$OWN_DIR/zrlmpi.cc1 $PY_ENV $3 $4 $HW_TARGET_C $HW_TARGET_H $SW_TARGET_C $SW_TARGET_H $6"
+  $OWN_DIR/zrlmpi.cc1 $PY_ENV $3 $4 $HW_TARGET_C $HW_TARGET_H $SW_TARGET_C $SW_TARGET_H $6
   fi
   ./zrlmpi.cc2 $1
   ./zrlmpi.cc3 $1
